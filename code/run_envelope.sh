a90c245eb180c633a4195641f72ad8f168ae7f91
cryptoshrine <166791466+cryptoshrine@users.noreply.github.com>
2024-06-23 12:32:59 +0100

    Update run_envelope.sh

--- code/run_envelope.sh
+++ code/run_envelope.sh
@@ -1,2 +1,2 @@
-source LiveTradingBots_Spot/code/.venv/bin/activate
-python3 LiveTradingBots_Spot/code/strategies/envelope/run.py
+source /home/ubuntu/LiveTradingBots_Spot/code/.venv/bin/activate
+python3 /home/ubuntu/LiveTradingBots_Spot/code/strategies/envelope/run.py
